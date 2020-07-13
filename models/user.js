8e9f192a133cbd874879895cf5d4bf4fa172f4b2
kushagraverma26 <kushagraverma26@gmail.com>
2020-07-14 00:23:39 +0530

    test

---
+++ models/user.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var user = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    
+    phone: {
+        type: Number, required: true
+    },
+    address: {
+        line1: { type: String, required: true },
+        line2: String,
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        pincode: { type: Number, required: true }
+
+    },
+    secret:{type: Object, required:true},
+    validated: {type:Boolean, default: false},
+
+    createdDate: { type: Date, default: Date.now }
+
+})
+
+module.exports = mongoose.model('Users',user)
---
+++ routes/auth.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config/secret');
+var users = require('../models/user');
+const speakeasy = require('speakeasy');
+const qrcode = require('qrcode');
+var router = express.Router()
+
+
+
+router.post("/registerUser", (req, res) => {
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  var secret = speakeasy.generateSecret({
+    name: "Testing"
+  });
+  var user = new users({
+    email: req.body.email,
+    password: hashedPassword,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    address: req.body.address,
+    secret: secret
+  })
+  var qr;
+  qrcode.toDataURL(secret.otpauth_url, function (err, data) {
+    qr = data;
+  });
+
+  user.save((err, newUser) => {
+    if (err) res.status(409).send(err)
+    else {
+      //var token = jwt.sign({ id: newUser._id }, config.secret, { expiresIn: 86400 });
+      res.send([newUser, { 'qrimage': qr }])
+    }
+  })
+})
+
+
+
+
+router.post("/verifyOtp", (req, res) => {
+  var secret;
+  var id;
+  var verified;
+  users.findOne({email: req.body.email},function(err,user){
+    if(err || user ==null){
+      console.log(err)
+      res.status(400).send("Bad Request")
+    }
+    else {
+    id = user._id;
+    secret = user.secret.ascii;
+    console.log(secret);
+    console.log("hello");
+
+    verified = speakeasy.totp.verify({
+      secret: user.secret.ascii,
+      encoding: "ascii",
+      token: req.body.otp,
+      window: 0
+    })
+
+      console.log(verified);
+      if (verified == true) {
+  
+        var token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 86400 });
+        res.send({
+          "valid": verified,
+          "token": token
+        })
+      }
+      else {
+        res.send({
+          "valid": verified
+        })
+      }
+   
+  }
+
+  })
+
+})
+
+
+
+module.exports = router
