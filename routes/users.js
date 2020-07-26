09522a98c9c9206721b1945f3981976ef0755bc6
kushagraverma26 <kushagraverma26@gmail.com>
2020-07-26 19:15:35 +0530

    keys

--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,29 @@ router.get("/myInfo", userValidate, (req, res) => {
 })
 
 
-//Token Validator
+
+
+// Store public key
+router.post("/generateKeys", userValidate, (req, res) => {
+    tokenToId(req.get("token")).then((id) => {
+        req.query['_id'] = id
+        users.findByIdAndUpdate(req.query, { $set: { publicKey: req.body.publicKey } }, { new: true }, function (err, user) {
+            if (err) {
+              res.status(500).send("DB error")
+            }
+            else {
+              res.send({"id": user._id, "firstName": user.firstName, "email": user.email, "publicKey": user.publicKey})
+            }
+          })
+
+
+    }).catch((err) => { res.status(403).send("Token Error") })
+
+
+})
+
+
+// Token Validator
 function userValidate(req, res, next) {
     tokenToId(req.get("token")).then((id) => {
         req.body.userId = id;
